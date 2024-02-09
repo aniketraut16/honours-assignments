3f7490e62536699ead9e2743bc32cb66bb496df7
aniketraut16 <rautan_1@rknec.edu>
2024-02-09 22:41:34 +0530

    Made isLeap function

---
+++ Req1/dateUtil.js
@@ -0,0 +1,13 @@
+function isLeap(year) {
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export { isLeap };
