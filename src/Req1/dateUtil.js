867025e2e7ddac72c01fd4d403db0f1269a17278
aniketraut16 <rautan_1@rknec.edu>
2024-02-14 15:38:24 +0530

    Done with Requirement 3

--- src/Req1/dateUtil.js
+++ src/Req1/dateUtil.js
@@ -9,5 +9,4 @@ function isLeap(year) {
     return true;
   }
 }
-
 export { isLeap };
--- src/Req2/poemreader.cjs
+++ src/Req2/poemreader.cjs
@@ -1,8 +1,5 @@
 const fs = require("node:fs");
 
-console.log("__dirname value:", __dirname);
-
-console.log("before anonymous callback function");
 fs.readFile(`${__dirname}/poem.txt`, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading the file:", err);
